98b4e4ca5f044c989ce53f1f809ca5d8570a7e83
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-12-29 23:06:52 +0100

    keyring demo

--- App.tsx
+++ App.tsx
@@ -1,164 +1,85 @@
-import {
-  CameraMode,
-  CameraType,
-  CameraView,
-  useCameraPermissions,
-} from "expo-camera";
-import { useRef, useState } from "react";
-import { Button, Pressable, StyleSheet, Text, View } from "react-native";
-import { Image } from "expo-image";
-import { AntDesign } from "@expo/vector-icons";
-import { Feather } from "@expo/vector-icons";
-import { FontAwesome6 } from "@expo/vector-icons";
+import * as React from 'react';
+import { SafeAreaView, StyleSheet, TextInput, View } from 'react-native';
+import * as Keychain from 'react-native-keychain';
+import { Card, Button } from 'react-native-paper';
+
 
 export default function App() {
-  const [permission, requestPermission] = useCameraPermissions();
-  const ref = useRef<CameraView>(null);
-  const [uri, setUri] = useState<string | null>(null);
-  const [mode, setMode] = useState<CameraMode>("picture");
-  const [facing, setFacing] = useState<CameraType>("back");
-  const [recording, setRecording] = useState(false);
-
-  if (!permission) {
-    return null;
-  }
-
-  if (!permission.granted) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: "center" }}>
-          We need your permission to use the camera
-        </Text>
-        <Button onPress={requestPermission} title="Grant permission" />
-      </View>
-    );
-  }
-
-  const takePicture = async () => {
-    const photo = await ref.current?.takePictureAsync();
-    setUri(photo?.uri);
-  };
-
-  const recordVideo = async () => {
-    if (recording) {
-      setRecording(false);
-      ref.current?.stopRecording();
-      return;
-    }
-    setRecording(true);
-    const video = await ref.current?.recordAsync();
-    console.log({ video });
-  };
-
-  const toggleMode = () => {
-    setMode((prev) => (prev === "picture" ? "video" : "picture"));
-  };
-
-  const toggleFacing = () => {
-    setFacing((prev) => (prev === "back" ? "front" : "back"));
-  };
-
-  const renderPicture = () => {
-    return (
-      <View>
-        <Image
-          source={{ uri }}
-          contentFit="contain"
-          style={{ width: 300, aspectRatio: 1 }}
-        />
-        <Button onPress={() => setUri(null)} title="Take another picture" />
-      </View>
-    );
-  };
-
-  const renderCamera = () => {
-    return (
-      <CameraView
-        style={styles.camera}
-        ref={ref}
-        mode={mode}
-        facing={facing}
-        mute={false}
-        responsiveOrientationWhenOrientationLocked
-      >
-        <View style={styles.shutterContainer}>
-          <Pressable onPress={toggleMode}>
-            {mode === "picture" ? (
-              <AntDesign name="picture" size={32} color="white" />
-            ) : (
-              <Feather name="video" size={32} color="white" />
-            )}
-          </Pressable>
-          <Pressable onPress={mode === "picture" ? takePicture : recordVideo}>
-            {({ pressed }) => (
-              <View
-                style={[
-                  styles.shutterBtn,
-                  {
-                    opacity: pressed ? 0.5 : 1,
-                  },
-                ]}
-              >
-                <View
-                  style={[
-                    styles.shutterBtnInner,
-                    {
-                      backgroundColor: mode === "picture" ? "white" : "red",
-                    },
-                  ]}
-                />
-              </View>
-            )}
-          </Pressable>
-          <Pressable onPress={toggleFacing}>
-            <FontAwesome6 name="rotate-left" size={32} color="white" />
-          </Pressable>
-        </View>
-      </CameraView>
-    );
-  };
 
   return (
-    <View style={styles.container}>
-      {uri ? renderPicture() : renderCamera()}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Card style={styles.card}>
+        <SetPassword />
+        <AuthButton />
+      </Card>
+    </SafeAreaView>
   );
 }
+
+
+const SetPassword = () => {
+  const [text, setText] = React.useState("initial");
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        keyboardType="phone-pad"
+        onChangeText={newText => setText(newText)}
+      />
+      <Button
+        mode="contained"
+        onPress={() => Keychain.setGenericPassword("com.Carpe.Password", text)}
+      > SUBMIT </ Button>
+    </View>
+  );
+};
+
+
+
+const getPass = async () => {
+  try {
+    // Retrieve the credentials
+    const credentials = await Keychain.getGenericPassword();
+    if (credentials) {
+      window.alert(
+        'Credentials successfully loaded for user :\n' + credentials.password
+      );
+    } else {
+      window.alert('No credentials stored');
+    }
+  } catch (error) {
+    console.error("Failed to access Keychain", error);
+  }
+};
+
+const AuthButton = () => (
+  <Button icon="key" style={styles.button} onPress={getPass}>
+    Reveal
+  </Button>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 20,
   },
-  camera: {
-    flex: 1,
+  card: {
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 20,
+  },
+  button: {
+    backgroundColor: '#eeeeee',
+  },
+  input: {
+    borderColor: "gray",
     width: "100%",
-  },
-  shutterContainer: {
-    position: "absolute",
-    bottom: 44,
-    left: 0,
-    width: "100%",
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 30,
-  },
-  shutterBtn: {
-    backgroundColor: "transparent",
-    borderWidth: 5,
-    borderColor: "white",
-    width: 85,
-    height: 85,
-    borderRadius: 45,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  shutterBtnInner: {
-    width: 70,
-    height: 70,
-    borderRadius: 50,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20
   },
 });
